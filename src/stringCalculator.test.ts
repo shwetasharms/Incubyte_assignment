bcbb2a4da09f5604d98372b400e1e5bec8dbce76
Shweta <shweta.sharma@thehigherpitch.com>
2024-06-19 21:26:14 +0530

    commit with the test return sum of two numbers

--- src/stringCalculator.test.ts
+++ src/stringCalculator.test.ts
@@ -1,8 +1,20 @@
 import {add} from "./stringCalculator"
+
+//  test case for empty string 
 test('return 0 for empty  string', ()=>{
     expect(add("")).toBe(0)
 })
+
+// test case for the single string 
 test('returns the number itself if the string contain the single number', ()=>{
-    expect(add('1')).toBe(1)
-    expect(add('5')).toBe(5)
+    expect(add("1")).toBe(1);
+    expect(add("5")).toBe(5);
 })
+
+
+// test case for the two numbers
+test('return the sum of two number', ()=>{
+    expect(add("1,2")).toBe(3);
+    expect(add("5,7")).toBe(12);
+})
+
--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,6 +1,6 @@
-export function add(numbers:string):number{
-    if(numbers==='') return 0;
-    if(!numbers.includes(","))return parseInt(numbers)
-    return 0;
+export function add(numbers: string): number {
+    if (numbers === '') return 0;
+    if (!numbers.includes(",")) return parseInt(numbers);
+    const numArray = numbers.split(",");
+    return numArray.reduce((sum,num)=>sum+parseInt(num), 0);
 }
-  
