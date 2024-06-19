72813b59bd98a4200a0ce204e0e578fe8ae733b2
Shweta <shweta.sharma@thehigherpitch.com>
2024-06-19 21:36:03 +0530

    commit with the test return sum of numbers for new line

--- src/stringCalculator.test.ts
+++ src/stringCalculator.test.ts
@@ -1,19 +1,19 @@
-import {add} from "./stringCalculator"
+import { add } from "./stringCalculator"
 
 //  test case for empty string 
-test('return 0 for empty  string', ()=>{
+test('return 0 for empty  string', () => {
     expect(add("")).toBe(0)
 })
 
 // test case for the single string 
-test('returns the number itself if the string contain the single number', ()=>{
+test('returns the number itself if the string contain the single number', () => {
     expect(add("1")).toBe(1);
     expect(add("5")).toBe(5);
 })
 
 
 // test case for the two numbers
-test('return the sum of two number', ()=>{
+test('return the sum of two number', () => {
     expect(add("1,2")).toBe(3);
     expect(add("5,7")).toBe(12);
 })
@@ -22,5 +22,10 @@ test('return the sum of two number', ()=>{
 test('returns the sum of multiple numbers ', () => {
     expect(add("1,2,3")).toBe(6);
     expect(add("5,7,8,2")).toBe(22);
-  });
-  
+});
+
+// test case for numbers separated by new lines
+test('returns the sum of numbers for  new lines', () => {
+    expect(add("1\n2,3")).toBe(6);
+    expect(add("5\n7\n8,2")).toBe(22);
+});
--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,6 +1,13 @@
 export function add(numbers: string): number {
+
+    // Return 0 if the input string  is empty
     if (numbers === '') return 0;
-    if (!numbers.includes(",")) return parseInt(numbers);
-    const numArray = numbers.split(",");
+
+    // delimiter is comma or newline
+    let delimiter=/,|\n/
+
+
+    // split the number  using delimeter
+    const numArray = numbers.split(delimiter);
     return numArray.reduce((sum,num)=>sum+parseInt(num), 0);
 }
