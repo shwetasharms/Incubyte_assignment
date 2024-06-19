f53aa3f9b0c6b1acd2926ba7a42b690b3724cd5f
Shweta <shweta.sharma@thehigherpitch.com>
2024-06-19 20:50:54 +0530

    commit with the test return number itself if the string contain the single number

--- src/stringCalculator.test.ts
+++ src/stringCalculator.test.ts
@@ -2,3 +2,7 @@ import {add} from "./stringCalculator"
 test('return 0 for empty  string', ()=>{
     expect(add("")).toBe(0)
 })
+test('returns the number itself if the string contain the single number', ()=>{
+    expect(add('1')).toBe(1)
+    expect(add('5')).toBe(5)
+})
--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,5 +1,6 @@
 export function add(numbers:string):number{
     if(numbers==='') return 0;
+    if(!numbers.includes(","))return parseInt(numbers)
     return 0;
 }
   
